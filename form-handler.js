ea24c792ba879ca7690af28771cfca11dfa3b971
Matthew Lal <3614683+matthewlal@users.noreply.github.com>
2018-06-01 22:39:05 -0400

    Update form-handler.js

--- form-handler.js
+++ form-handler.js
@@ -5,23 +5,23 @@ function loaded() {
 document.addEventListener("DOMContentLoaded", loaded, false);
 
 function handleFormSubmit(event) {
-  event.preventDefault(); 
+/**  event.preventDefault(); 
   var url = event.target.action;
   var xhr = new XMLHttpRequest();
   xhr.open('POST', url);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.onreadystatechange = function() {
     console.log(xhr.status, xhr.statusText)
-    console.log(xhr.responseText);  
+    console.log(xhr.responseText);  */
     document.getElementById("gform").style.display = "none"; // hide form
     var thankYouMessage = document.getElementById("thankyou_message");
     if (thankYouMessage) {
       thankYouMessage.style.display = "block";
     }
     return;
-  };
+  /**};
   var encoded = Object.keys(data).map(function(k) {
     return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
   }).join('&');
-  xhr.send(encoded);
+  xhr.send(encoded); */
 }
